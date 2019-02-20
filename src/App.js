38c2594806eceaaf472e122f44a35be228eef37d
Matthew Schaadt <matthew.schaadt@dealer.com>
2019-02-19 17:36:39 -0800

    clean up of initial development

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
+/* eslint-disable no-useless-escape */
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import moment from 'moment';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHatWizard, faCrow } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { spin: false };
+    console.log(`
+ ██████╗ ███████╗████████╗████████╗██╗███╗   ██╗    ███╗   ██╗██╗   ██╗████████╗███████╗██╗
+██╔════╝ ██╔════╝╚══██╔══╝╚══██╔══╝██║████╗  ██║    ████╗  ██║██║   ██║╚══██╔══╝██╔════╝██║
+██║  ███╗█████╗     ██║      ██║   ██║██╔██╗ ██║    ██╔██╗ ██║██║   ██║   ██║   ███████╗██║
+██║   ██║██╔══╝     ██║      ██║   ██║██║╚██╗██║    ██║╚██╗██║██║   ██║   ██║   ╚════██║╚═╝
+╚██████╔╝███████╗   ██║      ██║   ██║██║ ╚████║    ██║ ╚████║╚██████╔╝   ██║   ███████║██╗
+ ╚═════╝ ╚══════╝   ╚═╝      ╚═╝   ╚═╝╚═╝  ╚═══╝    ╚═╝  ╚═══╝ ╚═════╝    ╚═╝   ╚══════╝╚═╝
+                                                                                           
+               ___,,___                       
+          _,-='=- =-  -\`"--.__,,.._           
+       ,-;// /  - -       -   -= - "=.        
+     ,'///    -     -   -   =  - ==-=\`.      
+    |/// /  =    \`. - =   == - =.=_,,._ \`=/|  
+   ///    -   -    \  - - = ,ndDMHHMM/\b  \\  
+ ,' - / /        / /\ =  - /MM(,,._\`YQMML  \`| 
+<_,=^Kkm / / / / ///H|wnWWdMKKK#""-;. \`"0\  | 
+       \`""QkmmmmmnWMMM\""WHMKKMM\   \`--. \> \ 
+             \`""'  \`->>>    \`\`WHMb,.    \`-_<@)
+                               \`"QMM\`.        
+hjm                               \`>>>
+`);
+  }
+
   render() {
+    const {
+      spin,
+    } = this.state;
+    const now = moment();
+    const fellDate = moment('2015-03-09');
+    const years = now.diff(fellDate, 'years');
+    const months = now.diff(fellDate, 'months') % 12;
+    const lastMonth = now.clone().subtract(1, 'month');
+    const days = now.date() === 9 ? 0 : now.date() > 9 ? now.date() - 9 : now.diff(moment(`${lastMonth.year()}-${lastMonth.format('MM')}-09`), 'days');
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header className="App--header">
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <code>{years} years {months} months{days > 0 ? ` and ${days} days` : ''} </code>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <FontAwesomeIcon 
+            icon={faHatWizard}
+            className="App--hatWiz"
+          />
+         <FontAwesomeIcon
+            icon={faCrow}
+            className={`App--bird${spin ? '' : ' App--bird-offset'}`}
+            onClick={() => this.setState({ spin: !spin})}
+            spin={spin}
+          />
         </header>
       </div>
     );
